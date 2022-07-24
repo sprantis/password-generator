5dfc1f06797adc6af5b2334deca960fdcd868c34
Sarah Prantis <sprantis@gmail.com>
2022-07-24 18:38:55 -0400

    changes made to JS for function

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specChar =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 function generatePassword(){
-  var pwdLength = window.prompt("How long do you want your password to be? Please enter a number between 8-128.");
-    // need to validate number is between 8 and 128
+  var pwdLength = parseInt(window.prompt("How long do you want your password to be? Please enter a number between 8-128."));
+
+  if (pwdLength >= 8 && pwdLength <= 128){
+    window.alert("You've entered a valid password length.")
+  } else {
+    window.alert("Please enter a valid number between 8 and 128. Press the Generate Password button again to restart the process.")
+    return 
+  }
   var addNumbers = window.confirm("Do you want to add numbers to your password?");
   var addSpecChar = window.confirm("Do you want to add special characters to your password?");
   var addLowCase = window.confirm("Do you want lowercase letters in your password?");
@@ -19,34 +25,31 @@ function generatePassword(){
   var pwdOptions = [];
 
   // conditional statements to concatenate desired character options 
-  if (addNumbers) {
+  if (addNumbers === true) {
     pwdOptions = pwdOptions.concat(numbers);
   } 
   
-  if (addSpecChar) {
+  if (addSpecChar === true) {
     pwdOptions = pwdOptions.concat(specChar);
   }
   
-  if (addLowCase) {
+  if (addLowCase === true) {
     pwdOptions = pwdOptions.concat(letters);
   } 
 
-  if (addUppCase) {
+  if (addUppCase === true) {
     for ( let i = 0; i < letters.length; i++){
       upperLetters.push(letters[i].toUpperCase()); 
     }
     pwdOptions = pwdOptions.concat(upperLetters);
     
   }
- 
 
-  console.log(pwdOptions)
-
-
-
-  // for (let i = 0; i < pwdLength; i++){
-  //   // define and apply randomizer code to index of pwdOptions array
-  // }
+  for (let i = 0; i < pwdLength; i++){
+    // define and apply randomizer code to index of pwdOptions array
+    var randomNum = Math.floor(Math.random() * pwdOptions.length);
+    genPwd = genPwd.concat(pwdOptions[randomNum]);
+  }
 
   // at the end of this function, return a value NOT console.log
   return genPwd;
