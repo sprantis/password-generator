1abdb676325c0199ecaeefe2f3edbba7f0aba5dc
Sarah Prantis <sprantis@gmail.com>
2022-07-25 12:42:44 -0400

    edited comments

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ var specChar =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 function generatePassword(){
   var pwdLength = parseInt(window.prompt("How long do you want your password to be? Please enter a number between 8-128."));
 
-  // conditional statements that defines the paramenter of pwdlength within 8-128 and if a false input is entered user is instructed to restart the process. // 
+  // conditional statements to valiudate that user;s input a number betweeb 8-128 
+  //  if a false input is entered user is instructed to restart the process
   if (pwdLength >= 8 && pwdLength <= 128){
     window.alert("You've entered a valid password length.")
   } else {
@@ -17,17 +18,17 @@ function generatePassword(){
     return 
   }
 
-  // the user's input from the questions will be stored in the assigned variables. //
+  // the user's input from the questions will be stored in the assigned variables//
   var addNumbers = window.confirm("Do you want to add numbers to your password?");
   var addSpecChar = window.confirm("Do you want to add special characters to your password?");
   var addLowCase = window.confirm("Do you want lowercase letters in your password?");
   var addUppCase = window.confirm("Do you want Uppercase letters in your password?");
-//  two empy arrays and one empty string to store user's inputs for later use.//
+
   var upperLetters = [];
   var genPwd = "";
   var pwdOptions = [];
 
-  // conditional statements to concatenate desired character options //
+  // conditional statements to concatenate arrays based on user's input //
   if (addNumbers === true) {
     pwdOptions = pwdOptions.concat(numbers);
   } 
@@ -40,30 +41,25 @@ function generatePassword(){
     pwdOptions = pwdOptions.concat(letters);
   }
 
-//An if statement with a for loop within it that changes the letters[i] input to uppercase and stores in pwdOptions
-
+  // if user wants to include uppercase letters loop over letters array, convert to uppercase, then concatinate to pwdOptions
   if (addUppCase === true) {
     for ( let i = 0; i < letters.length; i++){
       upperLetters.push(letters[i].toUpperCase()); 
     }
     pwdOptions = pwdOptions.concat(upperLetters);
-    
   }
-  // for loop that multiples pwdOptions.length by Math.random within the Math.floor method and whatever the value is will be stored/assigned in genPwd variable.// 
-
+  // for loop that concatinates a value from pwdOptions using a random index
   for (let i = 0; i < pwdLength; i++){
-    
     var randomNum = Math.floor(Math.random() * pwdOptions.length);
     genPwd = genPwd.concat(pwdOptions[randomNum]);
   }
-// Return is to end the function with the value of genPwd//
+  // Return the value of genPwd//
   return genPwd;
 }
-
-// Get references to the #generate element
+// Get references to the #generate element within html
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input within html
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
